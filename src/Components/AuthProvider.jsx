31936f7d61fe956bbc87f4178b0c650b58d157b4
wdShopnil <shopnilabdullah2001@gmail.com>
2023-11-12 00:02:35 +0600

    token got successfully

--- src/Components/AuthProvider.jsx
+++ src/Components/AuthProvider.jsx
@@ -1,6 +1,7 @@
 import { createContext, useEffect, useState } from "react";
 import { createUserWithEmailAndPassword, getAuth, onAuthStateChanged, signInWithEmailAndPassword, signInWithPopup, signOut, updateProfile } from "firebase/auth"
 import app from "../Firebase/firebase.config";
+import axios from "axios";
 
 export const AuthContext = createContext(null);
 const auth = getAuth(app);
@@ -34,8 +35,23 @@ const AuthProvider = ({ children }) => {
     }
     useEffect(() => {
         const unSubscribe = onAuthStateChanged(auth, (currentUser) => {
+           const userEmail = currentUser?.email || user?.email
+           const loggedUser = {email: userEmail}
             setUser(currentUser);
             
+            if(currentUser){
+                axios.post('http://localhost:5000/jwt',loggedUser, {withCredentials:true})
+                .then(res=>{
+                    console.log(res.data);
+                })
+            }
+            else{
+                axios.post('http://localhost:5000/logout',loggedUser, {withCredentials:true})
+                .then(res=>{
+                    console.log(res.data);
+                })
+
+            }
             
             setLoading(false)
         });
